d7dd183cdd2ab756135c20026e297a1397b91114
Aldream <benjamin.planche@aldream.net>
2013-11-27 15:26:44 +0100

    <DB> Adding sqlite install to script

--- db/setUpDb.sh
+++ db/setUpDb.sh
@@ -1,4 +1,9 @@
 echo "---------------------------"
+echo "SQLite Install"
+echo "---------------------------"
+sudo apt-get install sqlite3
+
+echo "---------------------------"
 echo "Creation of the Database"
 echo "---------------------------"
 sqlite3 brightnest.db < createTables.sql
